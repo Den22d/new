c65edc9707e627c28a8d2ac130286d5d95456f2e
Den234k <d234k@ya.ru>
2025-01-03 23:14:29 +0600

    312

---
+++ pages/locators.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+class LoginPageLocators():
+    REG_LINK=(By.CSS_SELECTOR,"[value='Register']")
+    LOG_LINK=(By.CSS_SELECTOR,"[value='Log In']")
---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+﻿from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        x=self.browser.current_url
+        assert 'login' in x,"no login"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        
+        assert self.is_element_present(*LoginPageLocators.LOG_LINK),"NO Log form"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        
+        assert self.is_element_present(*LoginPageLocators.REG_LINL), "No Reg Form"
