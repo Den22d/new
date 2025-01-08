0beee24389fb7eec6c41a58493fce594873368ba
Den22d <d391898@ya.ru>
2025-01-08 18:06:17 +0500

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -9,7 +9,7 @@ from pages.basket_page import BasketPage
 
 from pages.test_login_page import LoginPage
 from pages.main_page import MainPage
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019r"
     page=ProductPage(browser, link)
@@ -43,12 +43,13 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+@pytest.mark.need_review    
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
 
@@ -80,7 +81,7 @@ class TestUserAddToBasketFromProductPage():
         #login_page = LoginPage(browser, browser.current_url)
 
         #login_page.should_be_login_page()
- 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self,browser):
         link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019r"
         page=ProductPage(browser, link)
